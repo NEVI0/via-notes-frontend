d50757d7167d5fb7badde492f08f77a4d11c0380
NEVI0 <neviocostamagagnin@gmail.com>
2021-05-12 12:54:13 -0300

    Adicionado o status context

---
+++ src/contexts/StatusContext.tsx
@@ -0,0 +1,40 @@
+import React, { useState,  createContext } from 'react';
+import { StatusType } from '../utils/types';
+
+import server from '../services/server';
+
+export interface StatusContextType {
+	statusArray: Array<StatusType>;
+	getStatus(): Promise<Array<StatusType>>;
+}
+
+const StatusContext: React.Context<StatusContextType | any> = createContext({});
+
+export const StatusProvider: React.FC = ({ children }) => {
+	
+	const [ statusArray, setStatusArray ] = useState<Array<StatusType>>([]);
+
+	const getStatus = async () => {
+		try {
+			const resp = await server.get(`/status`);
+			setStatusArray(resp.data.status);
+			return resp.data.status;
+		} catch (err) {
+			alert(err);
+		}
+	}
+
+	return (
+		<StatusContext.Provider
+			value={{
+				statusArray,
+				getStatus
+			}}
+		>
+			{ children }
+		</StatusContext.Provider>
+	);
+
+}
+
+export default StatusContext;
